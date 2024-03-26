1aa1010a84883c01ddd1d07e540cd1f2e9315861
JonelBriones <jonel.c.briones@gmail.com>
2024-03-26 01:28:59 -0700

    os appearance mode syncs with browser, light-dark-auto

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,36 +4,44 @@ import { IoBookOutline } from "react-icons/io5";
 import { PiList } from "react-icons/pi";
 import { GoHome } from "react-icons/go";
 import { MdOutlineCreate } from "react-icons/md";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { HiComputerDesktop } from "react-icons/hi2";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { FaRegMoon } from "react-icons/fa";
+import { TbBrightnessUp } from "react-icons/tb";
+
+let preferDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+let preferLight = window.matchMedia("(prefers-color-scheme: light)").matches;
 
 const Navbar = () => {
-  const [darkMode, setToggleMode] = useState("");
+  const [darkMode, setToggleMode] = useState(localStorage.theme);
   const [toggleDropdownSettings, setToggleDropdownSettings] = useState(false);
+  const [isDark, setIsDark] = useState(true);
 
   useEffect(() => {
-    if (darkMode === "") {
-      if (
-        localStorage.theme === "dark" ||
-        (!("theme" in localStorage) &&
-          window.matchMedia("(prefers-color-scheme: dark)").matches)
-      ) {
-        document.documentElement.classList.add("dark");
-      } else {
-        localStorage.removeItem("theme");
-      }
-    }
-
+    // automatically check os system preference
     if (
       localStorage.theme === "dark" ||
       (!("theme" in localStorage) &&
         window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
       document.documentElement.classList.add("dark");
-    }
-    if (darkMode === "light") {
+    } else if (
+      localStorage.theme === "light" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: light)").matches)
+    ) {
       document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
     }
+
+    // on user mode toggle
+
+    if (darkMode === "") localStorage.removeItem("theme");
+    if (darkMode === "dark") document.documentElement.classList.add("dark");
+    if (darkMode === "light") document.documentElement.classList.remove("dark");
   }, [darkMode]);
 
   const links = [
@@ -43,7 +51,7 @@ const Navbar = () => {
     "Report a problem",
     "Log out",
   ];
-  const [homeIconHover, setHomeIconHover] = useState(false);
+  const [appearanceShow, setAppearanceShow] = useState(false);
 
   return (
     <>
@@ -92,7 +100,7 @@ const Navbar = () => {
             <FaRegUser size={"2rem"} />
           </a>
         </div>
-        <div className="relative">
+        <div className="relative select-none">
           <a
             href="#"
             className="sm:w-10 px-6 py-3 dark:menu-hover"
@@ -101,21 +109,65 @@ const Navbar = () => {
             <PiList id="menu" size={"2rem"} />
           </a>
           {toggleDropdownSettings && (
-            <div className="flex flex-col bg-white dark:bg-neutral-900 w-44 shadow-lg absolute right-[-10px] rounded-lg">
-              <button className=" p-3 text-left rounded-t-lg font-[400]">
-                Appearance
-              </button>
-              {links.map((link) => (
-                <a className="p-3 border-t border-neutral-200 dark:border-neutral-600 font-[400]">
-                  {link}
-                </a>
-              ))}
+            <div className="flex flex-col bg-white dark:bg-neutral-900 w-44 shadow-lg absolute right-[-10px] rounded-lg mr-4">
+              {appearanceShow ? (
+                <div className="p-2">
+                  <div className="flex space-x-4 place-items-center">
+                    <IoMdArrowRoundBack
+                      onClick={() => setAppearanceShow(false)}
+                      className="cursor-pointer"
+                    />
+                    <span>Appearance</span>
+                  </div>
+                  <div className="flex justify-center">
+                    <div
+                      className="p-1 flex items-center cursor-pointer"
+                      onClick={() => setToggleMode("dark")}
+                    >
+                      <FaRegMoon color={darkMode === "dark" ? "white" : ""} />
+                    </div>
+                    <div
+                      className={`p-1 flex items-center cursor-pointer rounded-lg ${
+                        darkMode === "light" && "bg-neutral-900"
+                      } `}
+                      onClick={() => setToggleMode("light")}
+                    >
+                      <TbBrightnessUp
+                        color={darkMode === "light" ? "white" : ""}
+                      />
+                    </div>
+                    <div
+                      className={`p-1 flex items-center cursor-pointer rounded-lg ${
+                        darkMode === "" && "bg-neutral-900"
+                      } `}
+                      onClick={() => setToggleMode("")}
+                    >
+                      <HiComputerDesktop
+                        color={darkMode === "" ? "white" : ""}
+                      />
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <button
+                  className=" p-3 text-left rounded-t-lg font-[400]"
+                  onClick={() => setAppearanceShow(true)}
+                >
+                  Appearance
+                </button>
+              )}
+              {!appearanceShow &&
+                links.map((link) => (
+                  <a
+                    className="p-3 border-t border-neutral-200 dark:border-neutral-600 font-[400]"
+                    key={link}
+                  >
+                    {link}
+                  </a>
+                ))}
             </div>
           )}
         </div>
-
-        {/* <a href="#">Log in</a>
-      <a href="#">Sign up</a> */}
       </div>
       <button
         onClick={() => setToggleMode("dark")}
